96736a1c18118a92e06c0ed8a4c01d4a523f7ee0
Tim Johansson <tim.johansson@hiq.se>
2024-05-16 10:01:39 +0200

    Attempt at getting repo to work

--- services/subscription_manager/provision/entry.sh
+++ services/subscription_manager/provision/entry.sh
@@ -4,8 +4,8 @@ env_update /app/provision/_config.yml /app/provision/config.yml
 
 echo "Waiting for DB..."
 
-python /app/provision/provision_db.py
+python3.9 /app/provision/provision_db.py
 
 echo "Waiting for Broker..."
 
-python /app/provision/provision_broker.py
+python3.9 /app/provision/provision_broker.py
